482936587f689da92ae7c42ac1fa5af7b3b8208c
Alex Taylor <ataylo46@gmu.edu>
2022-09-05 15:18:25 -0400

    homework

--- Coding Exercise 1/Taylor_Exercise_Tidyverse.R
+++ Coding Exercise 1/Taylor_Exercise_Tidyverse.R
@@ -71,8 +71,77 @@ ratings
 
 # Problem 3 ---------------------------------------------------------------
 
-ratings %>% 
+m.mpaa <- ratings %>% 
   mutate(RATIO = BOX/BUDGET) %>% 
   group_by(MPAA) %>% 
   summarize_at(vars("BOX","BUDGET","RATIO"),mean) %>% 
+  rename(M.BOX = BOX, M.BUDGET = BUDGET, M.RATIO = RATIO) %>% 
   ungroup()
+m.mpaa
+
+# R rated movies recover more of their initial investment than movies with other ratings
+
+
+# Problem 4 ---------------------------------------------------------------
+
+ratings <- ratings %>% 
+  mutate(RATIO = BOX/BUDGET) %>% 
+  left_join(m.mpaa, by = "MPAA")
+ratings
+
+ratings <- ratings %>% 
+  group_by(MPAA) %>% 
+  mutate(SD.RATIO = sd(RATIO)) %>% 
+  ungroup()
+ratings
+
+ratings <- ratings %>% 
+  mutate(DIST.RATIO = abs(RATIO - M.RATIO)/SD.RATIO)
+
+# Top 6 Over and Under Performers
+
+g_perform <- ratings %>% 
+  filter(MPAA == "G") %>% 
+  arrange(desc(DIST.RATIO)) %>% 
+  slice(1:6)
+g_perform # Only 2 G-rated movies in dataset, equidistant from mean
+
+pg_perform <- ratings %>% 
+  filter(MPAA == "PG") %>% 
+  arrange(desc(DIST.RATIO)) %>% 
+  slice(1:6)
+pg_perform
+
+pg13_perform <- ratings %>% 
+  filter(MPAA == "PG13") %>% 
+  arrange(desc(DIST.RATIO)) %>% 
+  slice(1:6)
+pg13_perform
+
+r_perform <- ratings %>% 
+  filter(MPAA == "R") %>% 
+  arrange(desc(DIST.RATIO)) %>% 
+  slice(1:6)
+r_perform
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
